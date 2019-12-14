f3dae8de33104fc78aab9a74bd06a584cbc06b72
YEAP JO WEE <jowee@YEAPs-MacBook-Pro.local>
2019-12-14 17:02:50 +0800

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// The main function is the starting point of the Flutter app
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'I Am Rich',
+          ),
+          backgroundColor: Colors.pink[600],
+        ),
+      ),
+    ),
+  );
+}
